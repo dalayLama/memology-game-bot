fb4c04599e5223567a0d7d6245881d6018dbc6c5
regulyator <regulyator777@gmail.com>
2022-04-02 21:43:10 +0300

    init

---
+++ src/test/java/com/regulyator/memology/bot/memologygamebot/MemologyGameBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regulyator.memology.bot.memologygamebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemologyGameBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
